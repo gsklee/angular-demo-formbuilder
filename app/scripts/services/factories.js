3ee8c5a4ca9c9fdf72756ca3a1eac5d24038d1bc
Gias Kay Lee <balancetraveller+github@gmail.com>
2013-10-14 10:13:43 +0800

    Add input fields for form-wide settings and other overall improvements

--- app/scripts/services/factories.js
+++ app/scripts/services/factories.js
@@ -5,6 +5,10 @@ angular.module('angularDemoFormbuilderApp').
 factory('Form', function(
 ){
     return {
+        title: 'New Form',
+
+        description: '(What\'s this form about?)',
+
         maxComponent: 5,
 
         components: []
