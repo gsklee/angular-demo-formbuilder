68e7199af316ee26806f51aa968eb0fdb94a4b74
Gias Kay Lee <balancetraveller+github@gmail.com>
2013-10-17 10:06:39 +0800

    Generalize the `event.stopPropagation()` solution with another directive

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -9,4 +9,13 @@ directive('appNav', function(
         templateUrl: 'views/nav.html',
         replace: true
     };
+}).
+
+directive('appNoBubbling', function(
+){
+    return function(scope, element, attrs) {
+        element.on(attrs.appNoBubbling, function(event) {
+            event.stopPropagation();
+        });
+    };
 });
